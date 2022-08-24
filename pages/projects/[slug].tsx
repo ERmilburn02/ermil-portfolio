31ded2a8b55bd623e582775ba4b68bd3147ebf06
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-08-25 00:07:50 +0100

    embeds for blog & portfolio

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import md from "markdown-it";
+import Head from "next/head";
 
 export async function getStaticPaths() {
   const files = fs.readdirSync("projects");
@@ -38,9 +39,43 @@ export default function ProjectPage({
   content: string;
 }) {
   return (
-    <div className="prose mx-auto text-center">
-      <h1>{frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: md({html: true, xhtmlOut: true}).render(content)}} />
-    </div>
+    <>
+      <Head>
+        <meta name="og:description" content={frontmatter.metaDesc} />
+        <meta name="description" content={frontmatter.metaDesc} />
+        <meta property="og:description" content={frontmatter.metaDesc} />
+        <meta name="twitter:description" content={frontmatter.metaDesc} />
+        <meta name="keywords" content={frontmatter.tags.toString()} />
+        <meta
+          content={`https://ERmilburn02.com/` + frontmatter.socialImage}
+          property="og:image"
+        />
+        <meta
+          content={`https://ERmilburn02.com/` + frontmatter.socialImage}
+          property="og:image"
+        />
+        <meta
+          content={`https://ERmilburn02.com/` + frontmatter.socialImage}
+          property="twitter:image"
+        />
+        <meta name="og:title" content={frontmatter.metaTitle} />
+        <meta property="og:title" content={frontmatter.metaTitle} />
+        <meta name="twitter:title" content={frontmatter.metaTitle} />
+        <meta name="og:type" content="website" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@ERmilburn02" />
+        <meta name="og:site_name" content="ERmilburn02" />
+        <meta property="og:site_name" content="ERmilburn02" />
+      </Head>
+      <div className="prose mx-auto text-center">
+        <h1>{frontmatter.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: md({ html: true, xhtmlOut: true }).render(content),
+          }}
+        />
+      </div>
+    </>
   );
 }
