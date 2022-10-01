8dde4cc4338c9e9f41510eb5deaf00b96835e73c
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2022-10-01 10:15:31 +0100

    testing updates

--- pages/testing/index.tsx
+++ pages/testing/index.tsx
@@ -7,14 +7,32 @@
 //   codeUrl: "/unity/testing/testing.wasm",
 // });
 
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 
-const Model = dynamic(
-  () => import('../../components/model'),
-  { ssr: false }
-)
+// const Model = dynamic(
+//   () => import('../../components/model'),
+//   { ssr: false }
+// )
+
+import { TwitterTweetEmbed } from "react-twitter-embed";
+import useDarkMode from "../../hooks/useDarkMode";
 
 export default function testing() {
+  const [darkTheme, setDarkTheme] = useDarkMode();
+
   // return <Unity unityContext={unityContext} className="w-5/6 mx-auto" />;
-  return <Model _src='3Dmodels/Astronaut.glb' _alt='Astro' _poster='' _className='w-5/6 h-screen mx-auto'></Model>
+  // return <Model _src='3Dmodels/Astronaut.glb' _alt='Astro' _poster='' _className='w-5/6 h-screen mx-auto'></Model>
+  return (
+    <>
+      <TwitterTweetEmbed
+        tweetId={"1575925906811457536"}
+        options={{
+          theme: darkTheme ? "dark" : "light",
+          dnt: true,
+          align: "center",
+          conversation: false,
+        }}
+      />
+    </>
+  );
 }
