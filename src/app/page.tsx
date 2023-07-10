0217530cf6f4879dbf3f7797ebf6d3fdcc8f6bf8
Eliza Milburn <ERmilburn02@outlook.com>
2023-07-10 22:56:08 +0100

    Change HomePage to rewrite message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import MarkdownComponent from "@/components/Markdown";
-
-export default async function Home() {
-  const res = await fetch("http://localhost:3000/hello.mdx");
-  const markdown = await res.text();
-
+export default async function HomePage() {
   return (
     <>
-      <MarkdownComponent md={markdown} />
+      <div className="grid place-items-center h-full w-full">
+        <div className="lg:mx-16 text-center">
+          <h1 className="title-main">Website is being rewritten</h1>
+          <p className="lg:mx-16 mx-4 text-sm lg:text-base">
+            If you&apos;ve found this page, welcome. This is the rewrite of my
+            website. It is currently incomplete, but I will be working to re-add
+            all of the old functionality as fast as I can. Thank you for your
+            patience.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
