420af2278c3c33b244cdc7058a71993164ccb97e
Eliza Milburn <ERmilburn02@outlook.com>
2023-07-11 09:54:01 +0100

    hopefully fix project page temporarily

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -5,8 +5,13 @@ export default async function IndividualProjectPage({
 }: {
   params: { slug: string };
 }) {
+  // TODO: Temp code to get it working
+  let url = process.env.VERCEL_BRANCH_URL
+    ? process.env.VERCEL_BRANCH_URL
+    : "localhost:3000";
+
   // TODO: Pull from Supabase
-  const res = await fetch("http://localhost:3000/hello.mdx");
+  const res = await fetch(`http://${url}/hello.mdx`);
   const markdown = await res.text();
 
   return (
