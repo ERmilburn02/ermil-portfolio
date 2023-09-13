624f18d3e6aeb79bd26bb0bd1d5b29526fda9c3c
Eliza Milburn <ERmilburn02@outlook.com>
2023-09-13 22:32:29 +0100

    Remove Legacy Link component from Markdown

--- src/components/RemoteMarkdown.tsx
+++ src/components/RemoteMarkdown.tsx
@@ -3,7 +3,6 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 // TODO: We have to explicitly make an allow list of custom components.
 // TODO: Wrap Next components in custom components
-import NextLink, { LinkProps as NextLinkProps } from "next/link";
 import MarkdownImage, { MarkdownImageProps } from "./markdown/MarkdownImage";
 import MarkdownIframe, { MarkdownIframeProps } from "./markdown/MarkdownIframe";
 import MarkdownYouTube, {
@@ -14,7 +13,6 @@ import MarkdownCode, { MarkdownCodeProps } from "./markdown/MarkdownCode";
 
 export default async function RemoteMarkdownComponent({ md }: { md: string }) {
   const components: React.ComponentProps<typeof MDXProvider>["components"] = {
-    Link: (props: NextLinkProps) => <NextLink {...props} />,
     Image: (props: MarkdownImageProps) => <MarkdownImage {...props} />,
     Iframe: (props: MarkdownIframeProps) => <MarkdownIframe {...props} />,
     YouTube: (props: MarkdownYouTubeProps) => <MarkdownYouTube {...props} />,
